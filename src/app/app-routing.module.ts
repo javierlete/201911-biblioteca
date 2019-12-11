68ea838726dc452a480b9fa10092ac82f56083f5
Javier Lete <javier@eskura21.com>
2019-12-11 20:29:58 +0100

    Implementación de rutado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListadoComponent } from './listado/listado.component';
+import { DetallesComponent } from './detalles/detalles.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/listado', pathMatch: 'full' },
+  { path: 'listado', component: ListadoComponent },
+  { path: 'detalles', component: DetallesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
